98da5e4ee036234a2b5f997493fde0a55858c72c
Sergi Casquero <111904117+Sergi-CM@users.noreply.github.com>
2023-10-30 22:51:53 +0100

    Add User schema and create/update user functionality within onboarding page

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+
   images: {
     remotePatterns: [{ protocol: "https", hostname: "img.clerk.com" }],
   },
